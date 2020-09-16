8476d23c3325679a02d2ff19093ce27f8de90cba
thedukeofcool <61243936+thedukeofcool@users.noreply.github.com>
2020-09-16 12:53:52 -0400

    Create faster bootloader

--- boot.asm
+++ boot.asm
@@ -1,21 +1,30 @@
-[bits 32]
-global start
-extern _kernel_main
 
-section .mbHeader
-align 0x4
-
-MODULEALIGN equ  1<<0                  
-MEMINFO     equ  1<<1                  
-FLAGS       equ  MODULEALIGN | MEMINFO  
-MAGIC       equ    0x1BADB002           
-CHECKSUM    equ -(MAGIC + FLAGS)      
-
-MultiBootHeader:
-   dd MAGIC
-   dd FLAGS
-   dd CHECKSUM
-
-start:
-push ebx
-call _kernel_main
+.set ALIGN,    1<<0            
+.set MEMINFO,  1<<1             
+.set FLAGS,    ALIGN | MEMINFO  
+.set MAGIC,    0x1BADB002       
+.set CHECKSUM, -(MAGIC + FLAGS) 
+ 
+.section .multiboot
+.align 4
+.long MAGIC
+.long FLAGS
+.long CHECKSUM
+ 
+.section .bss
+.align 16
+stack_bottom:
+.skip 16384
+stack_top:
+ 
+.section .text
+.global _start
+.type _start, @function
+_start:
+	mov $stack_top, %esp
+	call kernel_main
+	cli
+1:	hlt
+	jmp 1b
+ 
+.size _start, . - _start
