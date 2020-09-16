8090dd99a3fb2f55f83c69cda68a77acfc5c8743
thedukeofcool <61243936+thedukeofcool@users.noreply.github.com>
2020-09-16 15:06:52 -0400

    Replace kernel

--- kernel.cpp
+++ kernel.cpp
@@ -1,70 +1,80 @@
+#include <stdbool.h>
 #include <stddef.h>
 #include <stdint.h>
 
-enum vga_color
-{
-  	COLOR_BLACK = 0,
-	COLOR_BLUE = 1,
-	COLOR_GREEN = 2,
-	COLOR_CYAN = 3,
-	COLOR_RED = 4,
-	COLOR_MAGENTA = 5,
-	COLOR_BROWN = 6,
-	COLOR_LIGHT_GREY = 7,
-	COLOR_DARK_GREY = 8,
-	COLOR_LIGHT_BLUE = 9,
-	COLOR_LIGHT_GREEN = 10,
-	COLOR_LIGHT_CYAN = 11,
-	COLOR_LIGHT_RED = 12,
-	COLOR_LIGHT_MAGENTA = 13,
-	COLOR_LIGHT_BROWN = 14,
-	COLOR_WHITE = 15,
+#if defined(__linux__)
+#error "Please use a cross-compiler to compile SharpOS."
+#endif
+
+#if !defined(__i386__)
+#error "Please use a C++ ix86-elf compiler to compile SharpOS."
+#endif
+
+#if !defined(__cplusplus)
+#error "Please use a C++ ix86-elf compiler to compile SharpOS."
+#endif
+
+enum vga_color {
+	VGA_COLOR_BLACK = 0x0,
+	VGA_COLOR_BLUE = 0x1,
+	VGA_COLOR_GREEN = 0x2,
+	VGA_COLOR_CYAN = 0x3,
+	VGA_COLOR_RED = 0x4,
+	VGA_COLOR_MAGENTA = 0x5,
+	VGA_COLOR_BROWN = 0x6,
+	VGA_COLOR_LIGHT_GRAY = 0x7,
+	VGA_COLOR_DARK_GRAY = 0x8,
+	VGA_COLOR_LIGHT_BLUE = 0x9,
+	VGA_COLOR_LIGHT_GREEN = 0xA,
+	VGA_COLOR_LIGHT_CYAN = 0xB,
+	VGA_COLOR_LIGHT_RED = 0xC,
+	VGA_COLOR_LIGHT_MAGENTA = 0xD,
+	VGA_COLOR_LIGHT_BROWN = 0xE,
+	VGA_COLOR_WHITE = 0xF,
 };
 
-uint8_t make_color(enum vga_color fg, enum vga_color bg)
+static inline uint8_t vga_entry_color(vga_color fg, enum vga_color bg)
 {
-  return fg | bg << 4;
+	return fg | bg << 4;
 }
 
-uint16_t makevgaentry(char c, uint8_t color)
+static inline uint16_t vga_entry(unsigned char uc, uint8_t color)
 {
-  uint16_t c16 = c;
-  uint16_t color16 = color;
-	return c16 | color16 << 8;
+	return (uint16_t)uc | (uint16_t)color << 8;
 }
 
 size_t strlen(const char* str)
 {
-	size_t ret = 0;
-	while ( str[ret] != 0 )
-		ret++;
-	return ret;
+	size_t len = 0;
+	while (str[len])
+		len++;
+	return len;
 }
 
 static const size_t VGA_WIDTH = 80;
-static const size_t VGA_HEIGHT = 24;
+static const size_t VGA_HEIGHT = 25;
 
 size_t terminal_row;
 size_t terminal_column;
 uint8_t terminal_color;
 uint16_t* terminal_buffer;
- 
-void terminal_initialize()
+
+void terminal_initialize(void)
 {
 	terminal_row = 0;
 	terminal_column = 0;
-	terminal_color = make_color(COLOR_LIGHT_GREY, COLOR_BLACK);
-	terminal_buffer = (uint16_t*) 0xB8000;
-	for ( size_t y = 0; y < VGA_HEIGHT; y++ )
+	terminal_color = vga_entry_color(VGA_COLOR_LIGHT_GRAY, VGA_COLOR_BLACK);
+	terminal_buffer = (uint16_t*)0xB8000;
+	for (size_t y = 0; y < VGA_HEIGHT; y++)
 	{
-		for ( size_t x = 0; x < VGA_WIDTH; x++ )
+		for (size_t x = 0; x < VGA_WIDTH; x++)
 		{
 			const size_t index = y * VGA_WIDTH + x;
-			terminal_buffer[index] = make_vgaentry(' ', terminal_color);
+			terminal_buffer[index] = vga_entry(' ', terminal_color);
 		}
 	}
 }
- 
+
 void terminal_setcolor(uint8_t color)
 {
 	terminal_color = color;
@@ -73,32 +83,34 @@ void terminal_setcolor(uint8_t color)
 void terminal_putentryat(char c, uint8_t color, size_t x, size_t y)
 {
 	const size_t index = y * VGA_WIDTH + x;
-	terminal_buffer[index] = make_vgaentry(c, color);
+	terminal_buffer[index] = vga_entry(c, color);
 }
- 
+
 void terminal_putchar(char c)
 {
 	terminal_putentryat(c, terminal_color, terminal_column, terminal_row);
-	if ( ++terminal_column == VGA_WIDTH )
+	if (++terminal_column == VGA_WIDTH)
 	{
 		terminal_column = 0;
-		if ( ++terminal_row == VGA_HEIGHT )
-		{
+		if (++terminal_row == VGA_HEIGHT)
 			terminal_row = 0;
-		}
 	}
 }
- 
-void terminal_writestring(const char* data)
+
+void terminal_write(const char* data, size_t size)
 {
-	size_t datalen = strlen(data);
-	for ( size_t i = 0; i < datalen; i++ )
+	for (size_t i = 0; i < size; i++)
 		terminal_putchar(data[i]);
 }
- 
-void kernel_main()
+
+void terminal_writestring(const char* data)
 {
-  terminal_initialize();
-  terminal_writestring("wellcome to my first operating system!");
-  for(; ;);
+	terminal_write(data, strlen(data));
+}
+
+void kernel_main(void)
+{
+	terminal_initialize();
+
+	terminal_writestring("Welcome to SharpOS.");
 }
